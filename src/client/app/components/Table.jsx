81e90139b2f698cd74aae320292724278ef7c622
paxarpp <pavelpp1974@gmail.com>
2018-05-05 14:40:42 +0300

    table style

--- src/client/app/components/Table.jsx
+++ src/client/app/components/Table.jsx
@@ -3,39 +3,70 @@ import PropTypes from 'prop-types';
 
 import fildNamePathList from './fildNamePathList';
 import RowTD from './RowTD';
+import PaginationButton from '../components/PaginationButton';
 
 export default class Table extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            stringOnPage: 10,
+            page: 1,
+        }
+    }
+    handlerPagination = (page) => {
+        this.setState({
+            page: page,
+        })
+    }
     render() {
         const { selectedCar, pathLists } = this.props;
-        return (
-            <table className="pathListSelectedCar">
-                <thead>
-                    <tr>
-                        <th data-field-name={'name'}>{fildNamePathList['name']}</th>
-                        <th data-field-name={'dateBegin'}>{fildNamePathList['dateBegin']}</th>
-                        <th data-field-name={'fuel'}>{fildNamePathList['fuel']}</th>
-                        <th data-field-name={'constFuelChange'}>{fildNamePathList['constFuelChange']}</th>
-                        <th data-field-name={'pathBegin'}>{fildNamePathList['pathBegin']}</th>
-                        <th data-field-name={'pathEnd'}>{fildNamePathList['pathEnd']}</th>
-                        <th data-field-name={'milleage'}>{fildNamePathList['milleage']}</th>
-                        <th data-field-name={'ConsumptionFactoryFuel'}>{fildNamePathList['ConsumptionFactoryFuel']}</th>
-                        <th data-field-name={'fuelBegin'}>{fildNamePathList['fuelBegin']}</th>
-                        <th data-field-name={'addFuel'}>{fildNamePathList['addFuel']}</th>
-                        <th data-field-name={'addFuelWinter'}>{fildNamePathList['addFuelWinter']}</th>
-                        <th data-field-name={'fuelEnd'}>{fildNamePathList['fuelEnd']}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    pathLists.filter(path => {
-                        return (path.name === selectedCar)
-                    }).map( path =>{ return (
-                    <RowTD path={path} key={path.dateBegin}/>
-                    )})
-                }
-                </tbody>
+        const { page, stringOnPage } = this.state;
 
-            </table>
+        const tempArr = pathLists.filter((elem, idx) => {
+            if (idx >= (page - 1) * stringOnPage && idx <= (page * stringOnPage) - 1) {
+                return elem
+            }
+        })
+        return (
+            <div className="pathListSelectedCar">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th style={{width: "15%"}}>{fildNamePathList['name']}</th>
+                            <th style={{width: "10%"}}>{fildNamePathList['dateBegin']}</th>
+                            <th style={{width: "5%"}}>{fildNamePathList['fuel']}</th>
+                            <th style={{width: "5%"}}>{fildNamePathList['constFuelChange']}</th>
+                            <th style={{width: "10%"}}>{fildNamePathList['pathBegin']}</th>
+                            <th style={{width: "10%"}}>{fildNamePathList['pathEnd']}</th>
+                            <th style={{width: "10%"}}>{fildNamePathList['milleage']}</th>
+                            <th style={{width: "10%"}}>{fildNamePathList['ConsumptionFactoryFuel']}</th>
+                            <th style={{width: "5%"}}>{fildNamePathList['fuelBegin']}</th>
+                            <th style={{width: "5%"}}>{fildNamePathList['addFuel']}</th>
+                            <th style={{width: "5%"}}>{fildNamePathList['addFuelWinter']}</th>
+                            <th style={{width: "10%"}}>{fildNamePathList['fuelEnd']}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            tempArr.filter(path => {
+                                return (path.name === selectedCar)
+                            }).map(path => {
+                                return (
+                                    <RowTD path={path} key={path.dateBegin} />
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+                <PaginationButton
+                    length={pathLists.filter(path => {
+                        return (path.name === selectedCar)
+                    }).length}
+                    page={page}
+                    stringOnPage={stringOnPage}
+                    handlerPagination={this.handlerPagination} />
+            </div>
+
         )
     }
 }
