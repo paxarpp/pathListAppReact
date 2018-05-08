af00289c25a20b8fd51b0bafcd7c46daf96fcb4a
paxarpp <34523588+paxarpp@users.noreply.github.com>
2018-05-09 00:43:13 +0300

    add выбор листа из таблицы

--- src/client/app/components/Table.jsx
+++ src/client/app/components/Table.jsx
@@ -6,42 +6,32 @@ import RowTH from './RowTH';
 import PaginationButton from '../components/PaginationButton';
 
 export default class Table extends Component {
-
-    handlerPagination = (page) => {
-        const { handlerP } = this.props;
-        handlerP(page)
-    }
-    handlerTable = (name) => {
-        const { handlerTableSort } = this.props;
-        handlerTableSort(name)
-    }
-    render() {
-        const { page, 
-                stringOnPage,
-                length,
-                tempArr,
-                reverse,
-                name } = this.props;
-        return (
-            <div className="pathListSelectedCar">
-                <table className="table">
-                    <thead>
-                        <RowTH handlerTable={this.handlerTable} reverse={ reverse } name={name} />
-                    </thead>
-                    <tbody>
-                        {tempArr.map(path => {
-                            return (
-                                <RowTD path={path} key={path.dateBegin} />
-                            )
-                        })}
-                    </tbody>
-                </table>
-                <PaginationButton
-                    length={length}
-                    page={page}
-                    stringOnPage={stringOnPage}
-                    handlerPagination={this.handlerPagination} />
-            </div>
-        )
-    }
-}
+                 handlerPagination = page => {
+                   const { handlerP } = this.props;
+                   handlerP(page);
+                 };
+                 handlerTable = name => {
+                   const { handlerTableSort } = this.props;
+                   handlerTableSort(name);
+                 };
+                 handlerSelect = select => () => {
+                     const { handlerTableSelect } = this.props;
+                     handlerTableSelect(select);
+                 };
+                 render() {
+                   const { page, stringOnPage, length, tempArr, reverse, name } = this.props;
+                   return <div className="pathListSelectedCar">
+                       <table className="table">
+                         <thead>
+                           <RowTH handlerTable={this.handlerTable} reverse={reverse} name={name} />
+                         </thead>
+                         <tbody>
+                           {tempArr.map(path => {
+                             return <RowTD path={path} key={path.dateBegin} handler={this.handlerSelect} />;
+                           })}
+                         </tbody>
+                       </table>
+                       <PaginationButton length={length} page={page} stringOnPage={stringOnPage} handlerPagination={this.handlerPagination} />
+                     </div>;
+                 }
+               }
--- src/client/app/containers/TableContainer.jsx
+++ src/client/app/containers/TableContainer.jsx
@@ -1,80 +1,92 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-import Table from '../components/Table';
-import paginationData from '../components/paginationData';
+import Table from "../components/Table";
+import paginationData from "../components/paginationData";
+import { infoPathToName } from "../actions/pathLists.js";
+class TableContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stringOnPage: 10,
+      page: 1,
+      pathListsCar: this.props.pathLists,
+      reverse: false,
+      name: ""
+    };
+  }
 
-export default class TableContainer extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            stringOnPage: 10,
-            page: 1,
-            pathListsCar: this.props.pathLists,
-            reverse: false,
-            name: '',
-        }
-    }
+  handlerPagination = page => {
+    this.setState({ page });
+  };
+  handlerTableSelect = select => {
+      const { pathInfo } = this.props;
+      pathInfo(select);
+  };
 
-    handlerPagination = (page) => {
+  componentWillReceiveProps({ pathLists }) {
+    this.setState(prev => ({
+      ...prev,
+      pathListsCar: pathLists
+    }));
+  }
+
+  handlerTableSort = name => {
+    if (name) {
+      const { reverse } = this.state;
+      if (reverse) {
+        const { pathListsCar } = this.state;
         this.setState({
-            page,
-        })
+          pathListsCar: pathListsCar.slice().sort((a, b) => {
+            if (b[name] > a[name]) {
+              return 1;
+            } else if (b[name] < a[name]) {
+              return -1;
+            } else return 0;
+          }),
+          reverse: !reverse,
+          name
+        });
+      } else {
+        const { pathListsCar } = this.state;
+        this.setState({
+          pathListsCar: pathListsCar.slice().sort((a, b) => {
+            if (b[name] > a[name]) {
+              return -1;
+            } else if (b[name] < a[name]) {
+              return 1;
+            } else return 0;
+          }),
+          reverse: !reverse,
+          name
+        });
+      }
     }
+  };
+  render() {
+    const { page, stringOnPage, pathListsCar, reverse, name } = this.state;
+    const dataArr = paginationData(page, stringOnPage, pathListsCar);
+    return (
+      <Table
+        name={name}
+        reverse={reverse}
+        page={page}
+        stringOnPage={stringOnPage}
+        length={pathListsCar.length}
+        tempArr={dataArr}
+        handlerP={this.handlerPagination}
+        handlerTableSort={this.handlerTableSort}
+        handlerTableSelect={this.handlerTableSelect}
+      />
+    );
+  }
+}
 
-    componentWillReceiveProps ({pathLists}){
-        this.setState(prev=>({
-            ...prev,
-            pathListsCar: pathLists
-        }))
-    }
-
-    handlerTableSort = (name) => {
-        if (name) {
-            const { reverse } = this.state;
-            if(reverse){
-                const { pathListsCar } = this.state;
-                this.setState({
-                    pathListsCar: pathListsCar.slice().sort((a, b) => {
-                        if (b[name] > a[name]) {
-                            return 1;
-                        } else if (b[name] < a[name]) {
-                            return -1;
-                        } else return 0;
-                    }),
-                    reverse: !(reverse),
-                    name
-                })
-            } else {
-                const { pathListsCar } = this.state;
-                this.setState({
-                    pathListsCar: pathListsCar.slice().sort((a, b) => {
-                        if (b[name] > a[name]) {
-                            return -1;
-                        } else if (b[name] < a[name]) {
-                            return 1;
-                        } else return 0;
-                    }),
-                    reverse: !(reverse),
-                    name
-                })
-            } 
-        }
-    }
-    render() {
-        const { page, stringOnPage, pathListsCar, reverse, name } = this.state;
-        const dataArr = paginationData(page, stringOnPage, pathListsCar);
-        return (
-            <Table
-                name={name}
-                reverse={reverse}
-                page={page}
-                stringOnPage={stringOnPage}
-                length={pathListsCar.length}
-                tempArr={dataArr}
-                handlerP={this.handlerPagination}
-                handlerTableSort={this.handlerTableSort}
-            />
-        )
+const mapDispatchToProps = (dispatch) => {
+    return {
+        pathInfo: (path) => infoPathToName(dispatch, path),
     }
 }
+
+export default connect(null, mapDispatchToProps)(TableContainer);
