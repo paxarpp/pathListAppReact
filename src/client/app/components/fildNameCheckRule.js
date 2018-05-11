2ee8a13fe2cedd3820e471b2a7e7bcd34238069a
paxarpp <pavelpp1974@gmail.com>
2018-05-11 13:40:32 +0300

    regexp change

--- src/client/app/components/fildNameCheckRule.js
+++ src/client/app/components/fildNameCheckRule.js
@@ -1,16 +1,16 @@
 const fildNameCheckRule = {
     name: /^\S/,
     fuel: /{AI}||{DT}/,
-    constFuelChange: /[0-9]{1,4}/,
-    dateBegin: /\S/,
-    pathBegin: /\d+/,
-    pathEnd: /\d+/,
+    constFuelChange: /^[0-9]{1,5}/,
+    dateBegin: /^[0-9]{4}\S[0-9]{2}\S[0-9]{2}$/,
+    pathBegin: /^\d+$/,
+    pathEnd: /^\d+$/,
     milleage: /^\d+$/,
-    fuelBegin: /\d+/,
-    fuelEnd: /\d+/,
-    addFuel: /\d+/,
-    deltaFuel: /\d+/,
+    fuelBegin: /^\d+/,
+    fuelEnd: /^\d+/,
+    addFuel: /^\d+/,
+    deltaFuel: /^\d+/,
     ConsumptionFactoryFuel:/\d+/,
-    addFuelWinter:/\d+/
+    addFuelWinter:/^\d+/
 }
 export default fildNameCheckRule;
