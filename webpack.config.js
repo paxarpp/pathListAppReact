bfe80a302a58c5d1090c6075a08c6d6c404f5907
ppp <p@p.com>
2018-06-01 16:34:06 +0300

    config change

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,38 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-    devtool: 'source-map',
-    entry: path.join(__dirname, 'src', 'client', 'app', 'index.jsx'),
-    output: {
-        path: path.join(__dirname, 'src', 'client', 'public'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-          filename: "[name].css",
-          chunkFilename: "[id].css"
-        })
-      ],
-    module: {
-        rules: [
-            {
-            test: /\.jsx?/,
-            include: path.join(__dirname, 'src', 'client', 'app'),
-            use: 'babel-loader'
-        },
-        {
-            test: /\.css$/,
-            use: [
-              MiniCssExtractPlugin.loader,
-              "css-loader"
-            ]
-          }
-    ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'src', 'client', 'public'),
-        inline:true,
-        port: 3000,
-        compress: true,
+  devtool: 'source-map',
+  entry: path.join(__dirname, 'src', 'client', 'app', 'index.jsx'),
+  output: {
+    path: path.join(__dirname, 'src', 'client', 'public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: path.join(__dirname, 'src', 'client', 'app'),
+        use: 'babel-loader'
       },
-      devtool: "source-map"   
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'src', 'client', 'public'),
+    inline: true,
+    port: 3000,
+    compress: true
+  }
 };
