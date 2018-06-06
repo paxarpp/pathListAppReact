7eb1a9397212dd8a9bead81b40edea0239239fe6
ppp <p@p.com>
2018-06-06 23:02:50 +0300

    add data for test component

---
+++ src/client/app/test/constDataTest.js
@@ -0,0 +1,147 @@
+const pathLists = [
+  {
+    ConsumptionFactoryFuel: 15.4,
+    addFuel: 0,
+    addFuelWinter: 0,
+    constFuelChange: 15.4,
+    dateBegin: '2018-05-03',
+    deltaFuel: 11.24,
+    fuel: 'AI',
+    fuelBegin: 40.47,
+    fuelEnd: 29.23,
+    milleage: 73,
+    name: 'уаз',
+    pathBegin: 116239,
+    pathEnd: 116312
+  },
+  {
+    ConsumptionFactoryFuel: 15.4,
+    addFuel: 45,
+    addFuelWinter: 0,
+    constFuelChange: 15.4,
+    dateBegin: '2018-05-04',
+    deltaFuel: 9.7,
+    fuel: 'AI',
+    fuelBegin: 29.2,
+    fuelEnd: 64.5,
+    milleage: 63,
+    name: 'уаз',
+    pathBegin: 116312,
+    pathEnd: 116375
+  },
+  {
+    ConsumptionFactoryFuel: 15.4,
+    addFuel: 0,
+    addFuelWinter: 0,
+    constFuelChange: 15.4,
+    dateBegin: '2018-05-07',
+    deltaFuel: 13.71,
+    fuel: 'AI',
+    fuelBegin: 64.5,
+    fuelEnd: 50.79,
+    milleage: 89,
+    name: 'уаз',
+    pathBegin: 116375,
+    pathEnd: 116464
+  },
+  {
+    ConsumptionFactoryFuel: 15.4,
+    addFuel: 136,
+    addFuelWinter: 0,
+    constFuelChange: 15.4,
+    dateBegin: '2018-05-08',
+    deltaFuel: 156.31,
+    fuel: 'AI',
+    fuelBegin: 50.79,
+    fuelEnd: 30.48,
+    milleage: 1015,
+    name: 'уаз',
+    pathBegin: 116464,
+    pathEnd: 117479
+  },
+  {
+    ConsumptionFactoryFuel: 15.4,
+    addFuel: 0,
+    addFuelWinter: 0,
+    constFuelChange: 15.4,
+    dateBegin: '2018-05-10',
+    deltaFuel: 11.24,
+    fuel: 'AI',
+    fuelBegin: 30.48,
+    fuelEnd: 19.24,
+    milleage: 73,
+    name: 'уаз',
+    pathBegin: 117479,
+    pathEnd: 117552
+  }
+];
+const cars = [
+  {
+    constFuelChange: '30.0',
+    fuel: 'DT',
+    name: 'маз'
+  },
+  {
+    constFuelChange: '35.0',
+    fuel: 'DT',
+    name: 'маз прицеп'
+  },
+  {
+    constFuelChange: '19.3',
+    fuel: 'DT',
+    name: 'валдай'
+  },
+  {
+    constFuelChange: '10.4',
+    fuel: 'DT',
+    name: 'фольц'
+  },
+  {
+    constFuelChange: '15.4',
+    fuel: 'AI',
+    name: 'уаз'
+  },
+  {
+    constFuelChange: '12.7',
+    fuel: 'AI',
+    name: 'волга'
+  }
+];
+const error = [
+  {
+    ConsumptionFactoryFuel: 15.4,
+    addFuel: 0,
+    addFuelWinter: 0,
+    constFuelChange: 15.4,
+    dateBegin: '2018-05-03',
+    deltaFuel: 11.24,
+    errorFuel: true,
+    errorPath: false,
+    first: true,
+    fuel: 'AI',
+    fuelBegin: 40.47,
+    fuelEnd: 29.23,
+    milleage: 73,
+    name: 'уаз',
+    pathBegin: 116239,
+    pathEnd: 116312
+  },
+  {
+    ConsumptionFactoryFuel: 15.4,
+    addFuel: 45,
+    addFuelWinter: 0,
+    constFuelChange: 15.4,
+    dateBegin: '2018-05-04',
+    deltaFuel: 9.7,
+    errorFuel: true,
+    errorPath: false,
+    first: false,
+    fuel: 'AI',
+    fuelBegin: 29.2,
+    fuelEnd: 64.5,
+    milleage: 63,
+    name: 'уаз',
+    pathBegin: 116312,
+    pathEnd: 116375
+  }
+];
